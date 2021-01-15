c4c04bd583d8a702eafce859d403f2fae3f20da2
devdrums <devdrums@macbook-pro.myfiosgateway.com>
2021-01-15 15:24:23 -0500

    imported models from postgres

--- chronograph/chronograph/settings.py
+++ chronograph/chronograph/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'reports.apps.ReportsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- chronograph/reports/models.py
+++ chronograph/reports/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    report = models.ForeignKey('Report', models.DO_NOTHING, blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    filetype = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'document'
+
+
+class Page(models.Model):
+    document = models.ForeignKey(Document, models.DO_NOTHING, blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+    footnote = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'page'
+
+
+class Report(models.Model):
+    title = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'report'
