7596f7f0ad493239dffdaa9fad580238389760fe
devdrums <devdrums@macbook-pro.myfiosgateway.com>
2021-01-15 23:00:28 -0500

    implemented query #3

--- chronograph/reports/urls.py
+++ chronograph/reports/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('getReportTitles', views.get_report_title, name='report_titles')
+  path('getReportTitles', views.get_report_title, name='report_titles'),
+  path('getPagesPercentage', views.get_pages_with_fn_pct, name='page_pct')
 ]
--- chronograph/reports/views.py
+++ chronograph/reports/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 from django.core import serializers
 from django.http import JsonResponse
-from django.db.models import Count
+from django.db.models import Count, Sum
 from .models import Report, Document, Page
 
 def get_report_title(request):
@@ -26,6 +26,7 @@ def get_report_title(request):
     GROUP BY
         R.TITLE;
     """
+
     documents = Document.objects.all();
 
     pages = Page.objects.filter(document_id__in=documents.values('id'))
@@ -35,3 +36,74 @@ def get_report_title(request):
         .annotate(pages=Count('page')))
 
     return JsonResponse(title_with_pages, safe=False)
+
+
+def get_pages_with_fn_pct(request):
+    """
+    SELECT
+        TP.DOC_ID,
+        TP.TOTAL_PAGES,
+        PWF.PAGES_WITH_FN,
+        ROUND(PWF.PAGES_WITH_FN / TP.TOTAL_PAGES::NUMERIC, 2) PERCENTAGE
+    FROM
+        (
+            SELECT 
+                D.ID AS DOC_ID,
+                COUNT(*) AS TOTAL_PAGES
+            FROM
+                DOCUMENT D,
+                PAGE P
+            WHERE
+                D.ID = P.DOCUMENT_ID
+            GROUP BY
+                DOC_ID
+            ORDER BY
+                DOC_ID ASC
+        ) TP,
+        (
+            SELECT
+                P.DOCUMENT_ID AS PAGE_DOC_ID,
+                COUNT(*) PAGES_WITH_FN
+            FROM
+                PAGE P
+            WHERE
+                P.FOOTNOTE IS NOT NULL
+            GROUP BY
+                PAGE_DOC_ID
+            ORDER BY
+                PAGE_DOC_ID
+        ) PWF
+    WHERE
+        TP.DOC_ID = PWF.PAGE_DOC_ID
+    GROUP BY
+        TP.DOC_ID,
+        TP.TOTAL_PAGES,
+        PWF.PAGES_WITH_FN;
+    """
+    documents = Document.objects.all();
+
+    total_pages = Page.objects.filter(document_id__in=documents.values('id')) \
+        .values('document_id') \
+        .annotate(total_pages=Count('document_id'))
+    
+    pages_with_footnote = Page.objects.filter(footnote__isnull=False) \
+        .values('document_id') \
+        .annotate(pages_with_fn=Count('document_id'))
+    
+    combined_pages = []
+
+    for tp_doc in total_pages:
+        tp_doc_id = tp_doc['document_id']
+
+        for pwf_doc in pages_with_footnote:
+            pwf_doc_id = pwf_doc['document_id']
+
+            if tp_doc_id == pwf_doc_id:
+                tp_doc['pages_with_fn'] = pwf_doc['pages_with_fn']
+                tp_doc['percentage'] = str(round(pwf_doc['pages_with_fn'] / tp_doc['total_pages'], 2) * 100) + '%'
+    
+        combined_pages.append(tp_doc)
+    
+    return JsonResponse(combined_pages, safe=False)
+
+    
